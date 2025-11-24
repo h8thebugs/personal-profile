1599918574883d95ae8a1fc089d358d793c2e066
JAS1CLJ <fixed-term.Szilard.Jakab@ro.bosch.com>
2025-11-24 18:13:46 +0200

    2025.11.24 JAS1CLJ added header and a language selection dropdown

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
+import { AboutPage } from './about-page/about-page';
+import { CvPage } from './cv-page/cv-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'about',
+    component: AboutPage
+  },
+  {
+    path: 'cv',
+    component: CvPage
+  }
+];
