a7ef6e5a60c2404e012feb094a4981dbbf31475d
JAS1CLJ <fixed-term.Szilard.Jakab@ro.bosch.com>
2025-11-26 17:57:49 +0200

    2025.11.26 JAS1CLJ finished about page, created scroll strategy, started cv page

--- src/app/cv-page/cv-page.ts
+++ src/app/cv-page/cv-page.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cv-page',
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './cv-page.html',
   styleUrl: './cv-page.scss',
 })
 export class CvPage {
+  slides = [
+    { title: 'Slide 1', content: 'Content 1' },
+    { title: 'Slide 2', content: 'Content 2' },
+    // Add more slides
+  ];
+  currentSlide = 0;
 
+  getBgTransform(i: number) {
+    const offset = (i - this.currentSlide) * 40; // slower movement
+    return `translateX(${offset}%)`;
+  }
+  getFgTransform(i: number) {
+    const offset = (i - this.currentSlide) * 100; // faster movement
+    return `translateX(${offset}%)`;
+  }
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
 }
