36f20f1c1afb011915abf4cc0567e2bab8624176
JAS1CLJ <fixed-term.Szilard.Jakab@ro.bosch.com>
2026-01-13 17:50:26 +0200

    2026.01.13 JAS1CLJ fix scroll

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Heading } from './heading/heading';
 
 @Component({
@@ -9,4 +9,12 @@ import { Heading } from './heading/heading';
   styleUrl: './app.scss'
 })
 export class App {
+ scrollContainer = viewChild.required<ElementRef<HTMLDivElement>>('scrollContainer');
+  constructor(private readonly router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.scrollContainer().nativeElement.scrollTo({ top: 0})
+      }
+    })
+  }
 }
