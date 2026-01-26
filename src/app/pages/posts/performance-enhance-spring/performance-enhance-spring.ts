bdfe8faf99c50654cf74da16cac28c886ab1001e
JAS1CLJ <fixed-term.Szilard.Jakab@ro.bosch.com>
2026-01-26 16:40:28 +0200

    2026.01.26 JAS1CLJ add english only notice, update metadata tags

--- src/app/pages/posts/performance-enhance-spring/performance-enhance-spring.ts
+++ src/app/pages/posts/performance-enhance-spring/performance-enhance-spring.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
+import { Meta } from '@angular/platform-browser';
+import { EnglishOnlyNotice } from 'src/app/components/english-only-notice/english-only-notice';
 
 @Component({
   selector: 'app-performance-enhance-spring',
-    imports: [
-        NgOptimizedImage
-    ],
+  imports: [
+    NgOptimizedImage,
+    EnglishOnlyNotice
+  ],
   templateUrl: './performance-enhance-spring.html',
   styleUrl: './performance-enhance-spring.scss',
 })
 export class PerformanceEnhanceSpring {
-
+  private readonly description = 'Sharing some of my experience regarding how to take over legacy Spring Boot applications, and what can one do to improve their performance';
+  constructor(private readonly meta: Meta) {
+    this.meta.updateTag(
+      {
+        property: 'og:title',
+        content: 'Enhance Performance in Legacy Spring Boot Apps'
+      }, 'property="og:title"'
+    );
+    this.meta.updateTag({
+      property: 'description',
+      content: this.description
+    }, 'name="description"');
+  }
 }
